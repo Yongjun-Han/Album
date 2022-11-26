c16850bbdbeacdaf1fc16b2661a77663f379492e
yongjunhan <hanyongjun.dev@gmail.com>
2022-11-26 23:55:44 +0900

    Initial Commit

---
+++ Album/AlbumApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumApp.swift
+//  Album
+//
+//  Created by YongJun Han on 2022/11/26.
+//
+
+import SwiftUI
+
+@main
+struct AlbumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
